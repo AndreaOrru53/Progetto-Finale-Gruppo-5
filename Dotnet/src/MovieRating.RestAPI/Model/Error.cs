2d8602467f8a94f01b178ff6f4dbc471ed73b803
Edivan Steri <edivansteri01@gmail.com>
2022-04-17 18:43:18 +0200

    aggiunta file necessari

---
+++ Dotnet/src/MovieRating.RestAPI/Model/Error.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace MovieRating.RestAPI.Model
+{
+    public record Error
+    {
+        [JsonPropertyName(name: "message")]
+        public string Message { get; set; }
+
+        [JsonPropertyName(name: "timestamp")]
+        public DateTime TimeStamp { get; set; }
+    }
+}
+
