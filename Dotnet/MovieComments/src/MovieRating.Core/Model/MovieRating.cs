dff6c12dc0bed6534874a2af5486d0ed75450811
Edivan Steri <edivansteri01@gmail.com>
2022-04-17 19:39:22 +0200

    aggiunta progetto

---
+++ Dotnet/MovieComments/src/MovieRating.Core/Model/MovieRating.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieRating.Core.Model
+{
+    public record Comment
+    {
+        public int id { get; set; }
+        public int user_id { get; set; }
+        public int movie_id { get; set; }
+        public string comment { get; set; }
+    }
+}
---
+++ Dotnet/MovieComments/src/MovieRating.Sql/DbContextManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MovieRating.DB.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieRating.DB
+{
+    public class DbContextManager : DbContext
+    {
+        public DbSet<MovieRatingEntity> Comments { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost;Port=3306;Database=movie_rating;Uid=root;";
+            var version = new MySqlServerVersion(new Version(10, 4, 22));
+
+            optionsBuilder.UseMySql(connectionString, version);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
+
+
+
