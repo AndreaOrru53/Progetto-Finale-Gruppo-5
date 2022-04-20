36bcc76e33dad7e2b0fadcc7d65ce236d411ba93
ANDREA ORRU <andreaorru@outlook.com>
2022-04-20 16:38:39 +0200

    add index

--- Node/index.js
+++ Node/index.js
@@ -4,6 +4,6 @@ const PORT = 1234;
 
 const app = express();
 
-app.listen(PORT, () => comsole.log(`Server listening: ${Port}`));
+app.listen(PORT, () => comsole.log(`Server listening: ${PORT}`));
 
 app.use(express.static('Frontend/index'));
