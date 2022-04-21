9d5ff835298a28dc5f682e7fe49ac67712c271b8
ANDREA ORRU <andreaorru@outlook.com>
2022-04-21 19:55:19 +0200

    add f

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular';
+  title = 'The Movie App';
+  isVisible = true;
 
 }
