cafca4ad2deed50c14127abb5c3db0436117bd67
Michele Lai <micklai2016@gmail.com>
2022-04-23 19:23:31 +0200

    working tmdb popular movies api

--- Angular/src/models/Movies.ts
+++ Angular/src/models/Movies.ts
@@ -1,51 +1,23 @@
 export interface Movies {
-    adult: boolean;
-    backdrop_path: string;
-    belongs_to_collection?: any;
-    budget: number;
-    genres: Genre[];
-    homepage: string;
-    id: number;
-    imdb_id: string;
-    original_language: string;
-    original_title: string;
-    overview: string;
-    popularity: number;
-    poster_path: string;
-    production_companies: Productioncompany[];
-    production_countries: Productioncountry[];
-    release_date: string;
-    revenue: number;
-    runtime: number;
-    spoken_languages: Spokenlanguage[];
-    status: string;
-    tagline: string;
-    title: string;
-    video: boolean;
-    vote_average: number;
-    vote_count: number;
-  }
-  
-  
-   interface Spokenlanguage {
-    english_name: string;
-    iso_639_1: string;
-    name: string;
-  }
-  
-   interface Productioncountry {
-    iso_3166_1: string;
-    name: string;
-  }
-  
-   interface Productioncompany {
-    id: number;
-    logo_path?: string;
-    name: string;
-    origin_country: string;
-  }
-  
-   interface Genre {
-    id: number;
-    name: string;
-  }
+  page: number;
+  results: Result[];
+  total_pages: number;
+  total_results: number;
+}
+
+interface Result {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
--- Angular/src/service/movie-api.service.ts
+++ Angular/src/service/movie-api.service.ts
@@ -9,12 +9,13 @@ import { MovieComment } from 'src/models/MovieComment';
 })
 export class MovieApiService {
 
-  apiKey: String = '1162250cf687d00d2cf98ee2b58e5093';
+  apiKey: string = '1162250cf687d00d2cf98ee2b58e5093';
+  apiUrl: string = 'https://api.themoviedb.org/3/movie/popular?api_key='
 
   constructor(private httpClient: HttpClient) { }
 
   getMovie(){
-    return this.httpClient.get<Movies>(`https://api.themoviedb.org/3/movie/550?api_key=${this.apiKey}`);
+    return this.httpClient.get<Movies>(`${this.apiUrl}${this.apiKey}`);
   }
 
 }
